5118a452b432a79b49d08e6af9fdc6df95148887
may-tech <aehkloprsw@gmail.com>
2020-04-15 18:07:55 +0900

    [add] text

---
+++ ch04/helloworld2.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+<!--    ドキュメントタイプの設定と実際のドキュメントの設定-->
+  <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+  </head>
+  <body>
+  <?php
+//    文字の出力
+      echo 'Hello World'.'<br>'."こんにちは世界";
+    ?>
+  </body>
+</html>

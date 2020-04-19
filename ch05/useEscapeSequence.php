c0e89080a475a33c563a690301ecd88ecb369368
may-tech <aehkloprsw@gmail.com>
2020-04-19 18:44:15 +0900

    [add] text

---
+++ ch05/useEscapeSequence.php
@@ -0,0 +1,21 @@
+<?php
+  $name="kanndahanako";
+  $height=154.6;
+  $age=36;
+  $savings=500;
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;sha-set=UTF-8">
+  </head>
+  <body>
+  <?php
+    echo "\t",$name,'さんの身長は',$height,"です。<br>\n";
+    echo "\t\t年齢は",$age,"歳です。<br>\n";
+    echo "\t\t30歳までに貯金¥",$savings,"万が目標です。<br>\n";
+    ?>
+  </body>
+</html>
+
+  

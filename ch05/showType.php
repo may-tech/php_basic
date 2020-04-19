7a8532d8589c5728a5c05104f742b8da01ebabb8
may-tech <aehkloprsw@gmail.com>
2020-04-19 18:08:49 +0900

    [add] text

---
+++ ch05/showType.php
@@ -0,0 +1,24 @@
+<?php
+  $name="kanndahanako";
+  $height=154.6;
+  $age=36;
+  $check=true;
+  $empty=null;
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;sha-set=UTF-8">
+  </head>
+  <body>
+  <?php
+    var_dump($name);echo"<br>";
+    var_dump($height);echo"<br>";
+    var_dump($age);echo"<br>";
+    var_dump($check);echo"<br>";
+    var_dump($empty);echo"<br>";
+    ?>
+  </body>
+</html>
+
+  

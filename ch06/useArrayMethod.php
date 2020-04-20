827ac14cae885d53c828d6c09eca169bfc9289e8
may-tech <aehkloprsw@gmail.com>
2020-04-20 12:55:40 +0900

    [add] text

---
+++ ch06/useArrayMethod.php
@@ -0,0 +1,17 @@
+<?php
+  $flowers=array('水仙','梅','沈丁花','桜','薔薇','紫陽花','朝顔','向日葵','彼岸花','金木犀','菊','胡蝶蘭');
+  $month=4;
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;sha-set=UTF-8">
+  </head>
+  <body>
+  <?php
+    echo "{$month}月に咲く代表的な花として、「{$flowers[$month-1]}」があります。";
+    ?>
+  </body>
+</html>
+
+  
